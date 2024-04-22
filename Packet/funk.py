7d746cfd40b40840b69b8127d995a62e7c977dff
User <t4rackanov.s@yandex.ru>
2024-04-22 21:40:40 +0300

    добавленна функция mult2,

--- Packet/funk.py
+++ Packet/funk.py
@@ -7,8 +7,12 @@ def minus(a, b):
 def mult(a, b):
     return a * b
 
+def mult2(a, b):
+    return a ** b
+
 
 if __name__ == '__main__':
     print(plus(2, 6))
     print(minus(2, 6))
     print(mult(2, 6))
+    print(mult2(2, 6))
